3d69d55161f2749b3a605945e683eb92ef6630e4
Boniface Masila <dietboni@gmail.com>
2025-11-20 08:54:13 +0300

    Fix duplicate import of API_BASE_URL in HospitalDashboard.jsx

--- frontend/src/components/HospitalDashboard.jsx
+++ frontend/src/components/HospitalDashboard.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import API_BASE_URL from "../config";
-import API_BASE_URL from "../config";
 
 const HospitalDashboard = ({ user }) => {
   const { t } = useTranslation();
